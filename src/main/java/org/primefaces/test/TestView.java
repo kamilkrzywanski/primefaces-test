e01060677c01d4d68f630208f13bc59712b05f3d
kkrzywanski <k.krzywanski@aasys.pl>
2025-05-21 12:33:05 +0200

    TAB_TIMELINE_BINDING

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,16 +1,21 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import lombok.Data;
+import org.primefaces.event.SelectEvent;
 
 @Data
 @Named
@@ -22,9 +27,11 @@ public class TestView implements Serializable {
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
     private List<TestObject> list;
+    private Date selectedDate;
 
     @PostConstruct
     public void init() {
+        selectedDate = new Date();
         string = "Welcome to PrimeFaces!!!";
         list = new ArrayList<>(Arrays.asList(
                 new TestObject("Thriller", "Michael Jackson", 1982),
@@ -34,4 +41,9 @@ public class TestView implements Serializable {
         ));
     }
 
+    public void onDateSelect(SelectEvent<Date> event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
 }
